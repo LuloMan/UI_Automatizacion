072e3f34754a51d836e30744e8b5c71b7f9f17a5
LuloMan <lucas.997@outlook.com>
2025-07-16 21:21:51 -0300

    Milestone_15

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,12 +1,20 @@
 package base;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 //import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
 import com.example.pages.HomePage;
+import com.google.common.io.Files;
 
 public class BaseTests {
     protected WebDriver driver;
@@ -27,5 +35,19 @@ public class BaseTests {
     @AfterClass
     public void tearDown(){
         //driver.quit(); //cierra la ventana y la sesion
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result){
+        var camera = (TakesScreenshot)driver;
+        if (result.getStatus() == ITestResult.FAILURE) {
+            try{
+                File screenshot = camera.getScreenshotAs(OutputType.FILE);
+                System.out.println("Screenshot taken: " + screenshot.getAbsolutePath());
+                Files.move(screenshot, new File("src/test/resources/screenshots/" + result.getName() + ".png"));
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
